82d210e7289d2b341eabb011000e4348b4fe39ac
Vinicius-Rubia <lucas.de.oliveira.rubia1@gmail.com>
2024-06-27 09:45:17 -0300

    backup project

---
+++ AwesomeDevEvents.API/Persistence/DevEventsDbContext.cs
@@ -0,0 +1,34 @@
+﻿using AwesomeDevEvents.API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace AwesomeDevEvents.API.Persistence
+{
+    public class DevEventsDbContext : DbContext
+    {
+        public DevEventsDbContext(DbContextOptions<DevEventsDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<DevEvent> DevEvents { get; set; }
+        public DbSet<DevEventSpeaker> DevEventSpeaker { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<DevEvent>(e =>
+            {
+                e.HasKey(k => k.Id);
+                e.Property(k => k.Title).IsRequired(false);
+                e.Property(k => k.Description).HasMaxLength(200).HasColumnType("varchar(200)");
+                e.Property(k => k.StartDate).HasColumnName("Start_Date");
+                e.Property(k => k.EndDate).HasColumnName("End_Date");
+                e.HasMany(k => k.Speakers).WithOne().HasForeignKey(fk => fk.DevEventId);
+            });
+
+            modelBuilder.Entity<DevEventSpeaker>(e =>
+            {
+                e.HasKey(k => k.Id);
+            });
+        }
+    }
+}
